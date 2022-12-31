76110ddc5f34643f19c59aabb0672648adbe6ecb
Leo Villalobos <villaleobos@gmail.com>
2022-12-31 10:59:19 -0800

    Scaffold methods for client

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,6 @@
 use std::{error, result};
 
+use crate::error::{ Error, Result };
 use crate::proto::{InputParcel, OutputParcel};
 
 use futures::stream::SplitStream;
@@ -15,5 +16,17 @@ pub struct Client {
 impl Client {
     pub fn new() -> Self {
         Self { id: Uuid::new_v4() }
+    }
+
+    pub fn read_input(&self, stream: SplitStream<WebSocket>) -> impl Stream<Item = Result<InputParcel>> {
+        todo!()
+    }
+
+    pub fn write_output<S, E>(&self, stream: S) -> impl Stream<Item = Result<warp::ws::Message>>
+    where
+        S: TryStream<Ok = OutputParcel, Error = E> + Stream<Item = result::Result<OutputParcel, E>>,
+        E: error::Error,
+    {
+        todo!()
     }
 }
