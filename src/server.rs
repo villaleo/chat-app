bd5040afd7eb2eaf0720bb39edda216885cb50c7
Leo Villalobos <villaleobos@gmail.com>
2022-12-31 11:06:41 -0800

    Implement process_client for server

--- src/server.rs
+++ src/server.rs
@@ -11,6 +11,7 @@ use tokio::sync::mpsc::UnboundedSender;
 use tokio::time::Duration;
 use warp::ws::WebSocket;
 use warp::Filter;
+use crate::client::Client;
 
 const ALIVE_INTERVAL_SECS: u64 = 5;
 
@@ -57,7 +58,34 @@ impl Server {
         }
     }
 
+    /// Describes the lifetime of a client.
     async fn process_client(hub: Arc<Hub>, web_socket: WebSocket, input_sender: UnboundedSender<InputParcel>) {
-        todo!()
+        let output_receiver = hub.subscribe();
+        let (ws_sink, ws_stream) = web_socket.split();
+        let client = Client::new();
+
+        info!("Client {} connected", client.id);
+
+        let reading = client.read_input(ws_stream).try_for_each(|input_parcel| async {
+            input_sender.send(input_parcel).unwrap();
+            Ok(())
+        });
+
+        let (tx, rx) = mpsc::unbounded_channel();
+        tokio::spawn(rx.forward(ws_sink));
+        let writing = client.write_output(output_receiver.into_stream()).try_for_each(|msg| async {
+            tx.send(Ok(msg)).unwrap();
+            Ok(())
+        });
+
+        if let Err(err) = tokio::select! {
+            result = reading => result,
+            result = writing => result,
+        } {
+            error!("Client connection error: {}", err);
+        }
+
+        hub.on_disconnect(client.id).await;
+        info!("Client {} disconnected", client.id);
     }
 }
