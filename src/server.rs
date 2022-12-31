a44cb32c99066294b67f6d29a1f3df69d982e660
Leo Villalobos <villaleobos@gmail.com>
2022-12-31 10:43:14 -0800

    Implement run method for server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::future::Future;
 use std::sync::Arc;
 
 use crate::hub::{ Hub, HubOptions };
@@ -26,5 +27,37 @@ impl Server {
                 alive_interval: Some(Duration::from_secs(ALIVE_INTERVAL_SECS)),
             })),
         }
+    }
+
+    pub async fn run(&self) {
+        let (input_sender, input_receiver) = mpsc::unbounded_channel::<InputParcel>();
+        let hub = self.hub.clone();
+
+        let feed = warp::path("feed")
+            .and(warp::ws())
+            .and(warp::any().map(move || input_sender.clone()))
+            .and(warp::any().map(move || hub.clone()))
+            .map(move |ws: warp::ws::Ws, input_sender, hub| {
+                // When a connection is established and upgraded to a WebSocket, delegate it to Self::process_client
+                ws.on_upgrade(move |web_socket| async move {
+                    tokio::spawn(Self::process_client(hub, web_socket, input_sender));
+                })
+            });
+
+        let shutdown = async {
+            tokio::signal::ctrl_c().await
+                .expect("failed to install CTRL+C signal handler");
+        };
+        let (_, serving) = warp::serve(feed).bind_with_graceful_shutdown(([127, 0, 0, 1], self.port), shutdown);
+        let running_hub = self.hub.run(input_receiver);
+
+        tokio::select! {
+            _ = serving => {},
+            _ = running_hub => {},
+        }
+    }
+
+    async fn process_client(hub: Arc<Hub>, web_socket: WebSocket, input_sender: UnboundedSender<InputParcel>) {
+        todo!()
     }
 }
