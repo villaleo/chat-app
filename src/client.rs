e072acc2123acb80f74f41907d450eabcee9ae82
Leo Villalobos <villaleobos@gmail.com>
2022-12-31 11:22:28 -0800

    Implement read_input for client

--- src/client.rs
+++ src/client.rs
@@ -7,6 +7,7 @@ use futures::stream::SplitStream;
 use futures::{future, Stream, StreamExt, TryStream, TryStreamExt};
 use uuid::Uuid;
 use warp::filters::ws::WebSocket;
+use warp::ws;
 
 #[derive(Clone, Copy, Default)]
 pub struct Client {
@@ -19,7 +20,25 @@ impl Client {
     }
 
     pub fn read_input(&self, stream: SplitStream<WebSocket>) -> impl Stream<Item = Result<InputParcel>> {
-        todo!()
+        let client_id = self.id;
+        // Only accept text messages
+        stream.take_while(|msg| {
+            future::ready(
+                if let Ok(message) = msg {
+                    message.is_text()
+                } else {
+                    false
+                }
+            )
+        })
+        .map(move |msg: result::Result<ws::Message, warp::Error>| match msg {
+            Err(err) => Err(Error::System(err.to_string())),
+            Ok(msg) => {
+                // Deserialize JSON into proto::Input
+                let input = serde_json::from_str(msg.to_str().unwrap())?;
+                Ok(InputParcel::new(client_id, input))
+            },
+        })
     }
 
     pub fn write_output<S, E>(&self, stream: S) -> impl Stream<Item = Result<warp::ws::Message>>
